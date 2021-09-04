4706b29394b67fad88c9cf4c91f62c331ec013cb
Rashish423 <60561400+Rashish423@users.noreply.github.com>
2021-09-04 13:39:52 +0530

    Add files via upload

---
+++ js/slider-index.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+
+	$('#js-main-slider').pogoSlider({
+		autoplay: true,
+		autoplayTimeout: 5000,
+		displayProgess: true,
+		preserveTargetSize: true,
+		targetWidth: 1000,
+		targetHeight: 300,
+		responsive: true
+	}).data('plugin_pogoSlider');
+
+	var transitionDemoOpts = {
+		displayProgess: false,
+		generateNav: false,
+		generateButtons: false
+	}
+
+});
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
